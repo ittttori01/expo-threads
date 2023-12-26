f47fc153b8b28431b52c3e57c641a88ebaf177ff
ittttori01 <ittttori01@gmail.com>
2023-12-27 08:49:47 +0900

    second

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,60 @@
+import * as React from "react";
 import Lottie from "lottie-react-native";
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+
+import {
+    Platform,
+    RefreshControl,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+} from "react-native";
 
 export default function TabOneScreen() {
+    const animationRef = React.useRef<Lottie>(null);
     return (
         <SafeAreaView>
-            <ScrollView contentContainerStyle={{ backgroundColor: "gray" }}>
+            <ScrollView
+                contentContainerStyle={{
+                    paddingHorizontal: 10,
+                    paddingTop: Platform.select({ android: 30 }),
+                }}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={false}
+                        onRefresh={() => {
+                            animationRef.current?.play();
+                        }}
+                        tintColor={"transparent"}
+                    />
+                }
+            >
                 <Lottie
-                    source={require("../../animation/Animation - 1703512434756.json")}
+                    ref={animationRef}
+                    source={require("../../animation/Animation - 1703631655289.json")}
                     autoPlay
-                    loop={true}
-                    style={{ width: 90, height: 90, alignSelf: "center" }}
+                    loop={false}
+                    style={{
+                        width: 90,
+                        height: 90,
+                        alignItems: "center",
+                    }}
                 />
             </ScrollView>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: "80%",
+    },
+});
